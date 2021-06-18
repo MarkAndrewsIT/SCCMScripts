deef8fc853668f49cd7fd1220e99197ee8966cfb
MarkAndrewsIT <86107535+MarkAndrewsIT@users.noreply.github.com>
2021-06-18 12:22:13 +0100

    Create EnterSCOMMaint.ps1

---
+++ EnterSCOMMaint.ps1
@@ -0,0 +1,22 @@
+<#
+This Powershell script is designed to be used with SCCM Orchestration Groups or standalone to put a server into Maintenance Mode in SCOM for 60 minutes
+To use set variable SCOMServer to your SCOM servers name, $User to an account with Operator Privileges in SCOM, and this account must be a member of the "Remote Management Users"
+group on the SCOM Server. Finally in $PWord enter the password for this account
+
+NOTE PSRemoting must be enabled on the SCOM Server
+#>
+
+$SCOMServer = "xxxx"
+$User = "domain\username"
+$PWord = ConvertTo-SecureString -String "password" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+
+$myFQDN=(Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+Invoke-Command -computername $SCOMServer -Credential $Credential -argumentlist $myFQDN -scriptblock {
+	param ($myFQDN)
+	Import-Module -Name OperationsManager
+	$Instance = Get-SCOMClassInstance -name $Using:myFQDN
+	$Time = ((Get-Date).AddMinutes(60))
+	Start-SCOMMaintenanceMode -Instance $Instance -EndTime $Time -Comment "Applying software update." -Reason "SecurityIssue"
+	}
+		
